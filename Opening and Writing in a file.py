4d1aa99ac21e040fce699a58d7202a0abf6064f3
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-08-26 18:39:31 +0530

    Update Opening and Writing in a file.py

--- Opening and Writing in a file.py
+++ Opening and Writing in a file.py
@@ -1,7 +1,14 @@
 #opening a file
 
-fileObject = open("file.txt","w")
+fileObject = open("trytwo.txt","w")
 
-#writing in the file
+#writing/editing a file
 
 fileObject.write("Hello World! Sup?")
+
+
+#adding multiple lines
+
+file_list = ["I am Hidayat\nHow are you?","Ok hello bye\nGoodnight"]
+
+fileObject.writelines(file_list)
