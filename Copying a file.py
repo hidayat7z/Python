cd6e030f5378453f673ecbf4a74919ae8efa29b1
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-08-27 19:04:40 +0530

    Update Copying a file.py

--- Copying a file.py
+++ Copying a file.py
@@ -5,3 +5,6 @@ import shutil
 
 # COPYING A FILE- shutil.copy("file_path","destination_path")
 shutil.copy("/home/hidayat7z/Documents/Nu2/NUfoldr/first.txt","/home/hidayat7z/")
+
+# COPYING MULTIPLE FILES: 
+# Make a list of files and then use shuti.copy() in a for loop and iterate over the list.
