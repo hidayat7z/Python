4279218b0db2f171d34adea5d69ade731827c4f2
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-08-27 17:41:26 +0530

    Update Opening, Writing and Close a file.py

--- Opening, Writing and Close a file.py
+++ Opening, Writing and Close a file.py
@@ -2,12 +2,12 @@
 
 fileObject = open("trytwo.txt","w")
 
-#writing/editing a file
+##writing/editing a file
 
 fileObject.write("Hello World! Sup?")
 
 
-#adding multiple lines
+##adding multiple lines
 
 file_list = ["I am Hidayat\nHow are you?","Ok hello bye\nGoodnight"]
 
@@ -16,22 +16,20 @@ fileObject.writelines(file_list)
 
 # APPENDING INFO TO ONE FILE FROM OTHER
 
-#open first file in append mode
+##open first file in append mode
 fileObject1 = open("first.txt",'a')
 
-#open second file in read mode
+##open second file in read mode
 fileObject2 = open("second.txt",'r')
 
-#read from second file   ((READING A FILE))
+##read from second file   ((READING A FILE))
 info= fileObject2.read()
 
-#append into first file
+##append into first file
 fileObject1.write(info)
 
 
-#CLOSING A FILE
+# CLOSING A FILE
 
 fileObject1.close()
 fileObject2.close()
-
-
