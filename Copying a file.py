cfd03284fb752e04bb05a8beab7dc4705b385861
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-08-27 18:07:13 +0530

    Create Copying a file.py

---
+++ Copying a file.py
@@ -0,0 +1,7 @@
+# COPYING A FILE
+
+import os
+import shutil
+
+# COPYING A FILE- shutil.copy("file_path","destination_path")
+shutil.copy("/home/hidayat7z/Documents/Nu2/NUfoldr/first.txt","/home/hidayat7z/")
