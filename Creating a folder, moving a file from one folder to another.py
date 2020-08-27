998063afd46f6f8e73c71bd15d8cef8298140c3c
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-08-27 17:54:11 +0530

    Update and rename Creating a folder.py to Creating a folder, moving a file from one folder to another.py

---
+++ Creating a folder, moving a file from one folder to another.py
@@ -0,0 +1,11 @@
+# MOVING A FILE
+
+import os
+import shutil
+
+##create a new directory (in Linux)
+os.mkdir("/home/hidayat7z/Documents/Nu foldr")
+
+
+#MOVING A FILE FROM ONE FOLDER TO ANOTHER
+shutil.move("/home/hidayat7z/first.txt","/home/hidayat7z/Documents/NUfoldr/first.txt")
--- Creating a folder.py
+++
@@ -1,7 +0,0 @@
-# MOVING A FILE
-
-import os
-import shutil
-
-##create a new directory (in Linux)
-os.mkdir("/home/hidayat7z/Documents/Nu foldr")
